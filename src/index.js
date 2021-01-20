93734fb01cbe3760d66d9fcac4b970639b58ff23
Violetta Ryabtsev <wondervioletta@gmail.com>
2021-01-20 15:33:52 -0800

    [add] video feature

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Player from './video';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Player />
   </React.StrictMode>,
   document.getElementById('root')
 );
